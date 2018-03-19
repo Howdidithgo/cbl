db8e4909a83789ef65b1badb1d3fab4daac031b7
Joy Paglinawan and Shane Frerichs <joy@>
2018-03-19 13:43:34 -0700

    add celsius to fahrenheit and fahrenheit to celsius programs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// this is the add function
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -7,6 +8,8 @@ var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
 
+// this is the BMI function
+
 var bmi = function(number1, number2) {
  return (number1 / (number2**2)) * 703;
 };
@@ -15,3 +18,23 @@ var bmi = function(number1, number2) {
  var number2 = parseInt(prompt("Enter height in inches:"));
  var result = bmi(number1, number2);
  alert(result);
+
+//celsius to fahrenheit
+
+ var far = function(number1) {
+ 	return (number1 +32) *(5/9)
+ };
+
+ var number1 = parseFloat(prompt("enter temp. in celcius:"));
+ var result = far(number1);
+ alert(result);
+
+// fahrenheit to celsius
+
+ var cel = function(number1) {
+	return (number1 -32) *(5/9)
+};
+
+var number1 = parseFloat(prompt("enter temp. in fahrenheit:"));
+var result = cel(number1);
+alert(result);
