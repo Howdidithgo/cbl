c7c77391401499d3e48779ba568a5d743d2ffc18
Joy Paglinawan and Shane Frerichs <joy@>
2018-03-19 12:57:32 -0700

    added bmi comp.

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,12 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
+
+var bmi = function(number1, number2) {
+ return (number1 / (number2**2)) * 703;
+};
+
+ var number1 = parseInt(prompt("Enter weight in pounds:"));
+ var number2 = parseInt(prompt("Enter height in inches:"));
+ var result = bmi(number1, number2);
+ alert(result);
